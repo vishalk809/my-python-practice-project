91344d465e752c31f63883d257acf07dc6b4683f
Vishal Kumar <vishalkum95082@gmail.com>
2026-07-10 12:33:36 +0530

    first commit

---
+++ lecture_two.py
@@ -0,0 +1,89 @@
+# str1 = "vishal" 
+# len1 = len(str1)
+
+# print(len1)
+
+# str2 = "kumar"
+# print(len(str2))
+
+# print(str1 + str2)
+
+# str = "vishal_kumar"
+# str[0]
+# print(str[9])
+# print(str[8:])
+# print(str[-5:-1])
+
+# print(str.endswith("ar"))
+
+# print(str.capitalize())
+
+# str = "i am studying python with youtub"
+
+# print(str.replace("studying", "learing"))
+
+# print(str.find("w"))
+
+# print(str.count("t"))
+
+# # pratice question
+# name = input("enter your name")
+# print("lengthn of your name is", len(name))
+
+# str = "hii, $iam the $ symbol $99.99"
+# print(str.count("$"))
+
+
+# if-elif-else(syntax)
+# age = 15
+
+# if(age >= 18):
+#     print("can vote")
+# if(age >= 21):
+#     print("can vote")    
+
+# elif(age >= 18):
+#     print("CANNOT vote")
+
+# light = "pink"
+
+# if(light == "red"):
+#     print("stop")
+# elif(light == "green"):
+#     print("go")
+# elif(light == "yellow"):
+#      print("look")
+
+# else:
+#     print("light is broken")
+
+# pratice question
+# marks = int(input("enter student marks : "))
+
+
+# if(marks >= 90):
+#     grade = "A"
+# elif(marks >= 80 and marks < 90):
+#     grade = "B"
+# elif(marks >= 70 and marks < 80):
+#     grade = "C"
+# else:
+#     grade = "D"
+
+# print("grade of the student ->", grade)
+
+# PRATICE QUESTION
+
+light = "green"
+
+if(light == "red"):
+    print("stop")
+elif(light == "yellow"):
+    print("running")
+elif(light == "green"):
+    print("start")
+else:
+    print("light is not avilable")
+
+
+
