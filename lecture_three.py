c831aa53a1c7c5ec0521f2cfdd02e48c52362483
Vishal Kumar <vishalkum95082@gmail.com>
2026-07-12 02:11:58 +0530

    first commit lecture_three.py

---
+++ lecture_three.py
@@ -0,0 +1,26 @@
+#  LISTS
+marks = [24.5, 88.5, 69.6, 45.2, 74.8]
+print(marks)
+print(len(marks))
+print(type(marks))
+print(marks[0])
+print(marks[1])
+
+
+student = ["vishal", 78.6, 21, "bagaha"]
+print(student)
+# //////////
+print(student[0])
+student[0] = "vikash"
+print(student)
+
+#  LISTS SLICING
+
+marks = [87, 85, 35, 65, 95]
+print(marks[4])
+print(marks[1:4])
+print(marks[:3])
+print(marks[1:])
+print(marks[-4:-1])
+
+
