b151500a106a0bce4b972dcf400d35dc3b6d9505
Vishal Kumar <vishalkum95082@gmail.com>
2026-07-13 11:34:21 +0530

    updated lecture_three.py

--- lecture_three.py
+++ lecture_three.py
@@ -1,26 +1,49 @@
 #  LISTS
-marks = [24.5, 88.5, 69.6, 45.2, 74.8]
-print(marks)
-print(len(marks))
-print(type(marks))
-print(marks[0])
-print(marks[1])
+# marks = [24.5, 88.5, 69.6, 45.2, 74.8]
+# print(marks)
+# print(len(marks))
+# print(type(marks))
+# print(marks[0])
+# print(marks[1])
 
 
-student = ["vishal", 78.6, 21, "bagaha"]
-print(student)
-# //////////
-print(student[0])
-student[0] = "vikash"
-print(student)
+# student = ["vishal", 78.6, 21, "bagaha"]
+# print(student)
+# # //////////
+# print(student[0])
+# student[0] = "vikash"
+# print(student)
 
-#  LISTS SLICING
+# #  LISTS SLICING
 
-marks = [87, 85, 35, 65, 95]
-print(marks[4])
-print(marks[1:4])
-print(marks[:3])
-print(marks[1:])
-print(marks[-4:-1])
+# marks = [87, 85, 35, 65, 95]
+# print(marks[4])
+# print(marks[1:4])
+# print(marks[:3])
+# print(marks[1:])
+# print(marks[-4:-1])
+
+# LISTS METHODS
+
+# list = [2, 1, 3]
+
+# list.append(4)
+
+# list.sort()
+
+#  list.sort(reverse=True)
+
+#  list.reverse()
+
+# list.insert(1, 5)
+
+#  list.remove(1)
+
+# list.pop(2)
+
+# print(list)
 
 
+#   TUPLES IN PYTHON
+
+  
