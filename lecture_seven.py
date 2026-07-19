33a96f4a405226713911bd31d57afc5cfe349367
Vishal Kumar <vishalkum95082@gmail.com>
2026-07-20 01:30:55 +0530

    updated lecture_seven.py

---
+++ lecture_seven.py
@@ -0,0 +1,78 @@
+# FILE I/O ( INPUT / OUTPUT)
+
+f = open("demo.txt", "r")
+data = f.read()
+print(data)
+print(type(data))
+f.close()
+ 
+#  READING A FILE
+
+f = open("demo.txt", "r")
+
+line1 = f.readline()
+print(line1)
+
+line2 = f.readline()
+print(line2)
+
+f.close()
+ 
+#  WRITING TO A FILE
+
+f = open("demo.txt", "w")
+
+f.write("i want to learn javascript")
+
+f.close()
+
+# /////////
+f = open("demo.txt", "a")
+
+f.write("\n I love javascript")
+
+f.close()
+
+#  WITH SYNTAX
+with open("demo.txt", "r") as f:
+    data = f.read()
+    print(data)
+
+# with open("demo.txt", "w") as f:
+#     f.write("\n new data")    
+
+#  DELETING A FILE
+# import os
+
+# os.remove("file name")
+
+# PRACTICE QUESTION  //////////////////
+# 1.CREATE A NEW FILE "PRACTICE.TXT" USING PYTHON. ADD THE FOLLOWING DATA IN IT:
+
+with open("practice.txt", "w") as f:
+    f.write("hi everyone\n we are learning file I/O\n")
+    f.write("using java.\n i like programming in java.")
+
+# 2. WAF THAT REPLACES ALL OCCURENCES OF "JAVA" WITH "PYTHON" IN ABOVE FILE.
+with open("practice.txt", "r") as f:
+    data = f.read()
+
+new_data = data.replace("java", "python")
+print(new_data)    
+
+with open("practice.txt", "w") as f:
+    f.write(new_data)
+
+# SEARCH IF THE WORD "LEARNING" EXISTS IN THE FILE OR NOT.
+def check_for_word():
+    word = "learning"
+    with open("practice.txt", "r") as f:
+        data = f.read()
+        if(data.find(word) != -1):
+            print("Found")
+        else:
+            print("not found")   
+
+check_for_word()
+            
+
